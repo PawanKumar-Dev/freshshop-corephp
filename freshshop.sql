4c92688c9e7977c2f174f3826516cc19a0678e91
Pawan <pk687376@gmail.com>
2022-03-19 20:44:56 +0530

    updated

---
+++ freshshop.sql
@@ -0,0 +1,209 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 18, 2022 at 08:33 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `freshshop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aboutpage`
+--
+
+CREATE TABLE `aboutpage` (
+  `id` int(11) NOT NULL,
+  `main_img` varchar(255) NOT NULL,
+  `main_text` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `aboutpage`
+--
+
+INSERT INTO `aboutpage` (`id`, `main_img`, `main_text`) VALUES
+(1, 'about-img.jpg', '\"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur.\r\n\r\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `banners`
+--
+
+CREATE TABLE `banners` (
+  `id` int(11) NOT NULL,
+  `headline` varchar(255) NOT NULL,
+  `quotes` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `banners`
+--
+
+INSERT INTO `banners` (`id`, `headline`, `quotes`) VALUES
+(1, 'Welcome To \nFreshshop', 'See how your users experience your website in realtime or view\r\ntrends to see any changes in performance over time.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contactpage`
+--
+
+CREATE TABLE `contactpage` (
+  `id` int(11) NOT NULL,
+  `info` text NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `contactpage`
+--
+
+INSERT INTO `contactpage` (`id`, `info`, `address`, `phone`, `email`) VALUES
+(1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent urna diam, maximus ut ullamcorper quis, placerat id eros. Duis semper justo sed condimentum rutrum. Nunc tristique purus turpis. Maecenas vulputate.', 'Michael I. Days 9000 Preston Street Wichita, KS 87213', '+1- 888 705 770', 'contactinfo@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `category` varchar(255) NOT NULL,
+  `price` int(11) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `image_link` varchar(255) NOT NULL,
+  `created_at` date NOT NULL DEFAULT current_timestamp(),
+  `updated_at` date NOT NULL DEFAULT current_timestamp(),
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `product_name`, `description`, `category`, `price`, `stock`, `image_link`, `created_at`, `updated_at`, `user_id`) VALUES
+(3, 'Apple', 'Nam sagittis a augue eget scelerisque. Nullam lacinia consectetur sagittis.', 'Fruit', 40, 40, 'img-pro-03.jpg', '2022-03-17', '2022-03-17', 1),
+(5, 'Guave', 'Nam sagittis a augue eget scelerisque. Nullam lacinia consectetur sagittis.', 'Fruits', 60, 40, 'img-pro-01.jpg', '2022-03-17', '2022-03-17', 1),
+(6, 'Chilles', 'Nam sagittis a augue eget scelerisque. Nullam lacinia consectetur sagittis.', 'Vegetable', 170, 45, 'img-pro-03.jpg', '2022-03-17', '2022-03-17', 1),
+(7, 'Oranges', 'Nam sagittis a augue eget scelerisque. Nullam lacinia consectetur sagittis.', 'Fruits', 74, 12, 'img-pro-01.jpg', '2022-03-17', '2022-03-17', 1),
+(8, 'Grapes', 'Nam sagittis a augue eget scelerisque. Nullam lacinia consectetur sagittis.', 'Fruits', 90, 48, 'img-pro-02.jpg', '2022-03-17', '2022-03-17', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(60) NOT NULL,
+  `created_at` date NOT NULL DEFAULT current_timestamp(),
+  `updated_at` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `name`, `email`, `password`, `role`, `created_at`, `updated_at`) VALUES
+(1, 'admin', 'Pawan', 'pk@gmail.com', '$2y$10$2heUeuHslXriiiwJFHbSLenUm1uX5YaaP069p7O7d0QSaBpL5WOAe', 'admin', '2022-03-17', '2022-03-17'),
+(2, 'pawan', 'pawan k', 'pk687@gmail.com', '$2y$10$qsodZFbJ.CA/WuGd4Az.deYRisECgoWfOUPSexxAJg9OYaz9WJf6e', 'user', '2022-03-17', '2022-03-17');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `aboutpage`
+--
+ALTER TABLE `aboutpage`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `banners`
+--
+ALTER TABLE `banners`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `contactpage`
+--
+ALTER TABLE `contactpage`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `aboutpage`
+--
+ALTER TABLE `aboutpage`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `banners`
+--
+ALTER TABLE `banners`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `contactpage`
+--
+ALTER TABLE `contactpage`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
