9c25f93c1aac1467e0a2cd4dc951e4baa0b36b9b
Pawan <pk687376@gmail.com>
2022-03-19 20:46:27 +0530

    sql upped

--- sqlFile/freshshop.sql
+++ sqlFile/freshshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 18, 2022 at 08:33 AM
+-- Generation Time: Mar 19, 2022 at 04:15 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
